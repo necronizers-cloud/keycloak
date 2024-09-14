2fd5c2f9981194d24113df0135eccec0262a478c
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2024-09-14 13:38:06 +0530

    Keycloak Cluster Deployment onto Self Hosted K3d Cluster

--- namespace/providers.tf
+++ namespace/providers.tf
@@ -13,12 +13,11 @@ terraform {
 
   backend "kubernetes" {
     secret_suffix = "namespace.keycloak"
-    config_path   = "~/.kube/config"
   }
 }
 
 provider "kubernetes" {
-  config_path = "~/.kube/config"
+
 }
 
 provider "null" {
