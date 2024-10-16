4f15be3302b1e65c6b17072502d33cb91e104ffd
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2024-10-16 15:44:51 +0530

    Move Keycloak Operator deployment and run it as a script (#18)
    * Removed null resource block
    
    * Keycloak Operator outside of TF

--- namespace/main.tf
+++ namespace/main.tf
@@ -8,20 +8,3 @@ resource "kubernetes_namespace" "keycloak" {
     }
   }
 }
-
-resource "null_resource" "install_crds" {
-  triggers = {
-    "install_crds" : true
-  }
-
-  provisioner "local-exec" {
-    command = <<EOT
-kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/25.0.4/kubernetes/keycloaks.k8s.keycloak.org-v1.yml
-kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/25.0.4/kubernetes/keycloakrealmimports.k8s.keycloak.org-v1.yml
-kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/25.0.4/kubernetes/kubernetes.yml -n ${var.namespace}
-sleep 60
-    EOT
-  }
-
-  depends_on = [kubernetes_namespace.keycloak]
-}
