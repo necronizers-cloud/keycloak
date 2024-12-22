261b4b2b44c7c20f440580be39924f5f95d096ac
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2024-12-22 13:10:38 +0530

    Keycloak Ingress-Backend TLS Setup (#28)

--- cluster/main.tf
+++ cluster/main.tf
@@ -97,7 +97,9 @@ resource "kubernetes_ingress_v1" "keycloak_ingress" {
       component = "ingress"
     }
     annotations = {
-      "nginx.ingress.kubernetes.io/proxy-ssl-verify" : "off"
+      "nginx.ingress.kubernetes.io/proxy-ssl-verify" : "on"
+      "nginx.ingress.kubernetes.io/proxy-ssl-secret" : "keycloak/keycloak-tls"
+      "nginx.ingress.kubernetes.io/proxy-ssl-name" : "keycloak-cluster-service.keycloak.svc.cluster.local"
       "nginx.ingress.kubernetes.io/backend-protocol" : "HTTPS"
       "nginx.ingress.kubernetes.io/rewrite-target" : "/"
       "nginx.ingress.kubernetes.io/proxy-body-size" : 0
